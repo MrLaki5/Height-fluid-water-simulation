a211c8e51922bfcfa75e33cb3a34c09f1a789693
MrLaki5 <mimar1207@gmail.com>
2019-09-07 13:54:11 +0200

    added scrypt for mouse click

---
+++ Height_fluid_water_simulation/Assets/ClickTrace.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ClickTrace : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    { 
+
+        if (!Input.GetMouseButton(0))
+            return;
+
+        RaycastHit hit;
+        if (!Physics.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition), out hit))
+            return;
+
+        Renderer renderer = hit.collider.GetComponent<Renderer>();
+        Texture mainTexture = renderer.sharedMaterial.GetTexture("_Tex");
+        MeshCollider meshCollider = hit.collider as MeshCollider;
+        if (renderer == null || renderer.sharedMaterial == null || mainTexture == null || meshCollider == null)
+            return;
+
+        Vector2 pixelUV = hit.textureCoord;
+        print((int)(pixelUV.x * mainTexture.width) + "--" + (int)(pixelUV.y * mainTexture.height));
+    }
+}
